b79d0969092ccba628635574461bd7d4fd9c92e7
vitasok797 <vitasok@gmail.com>
2025-10-03 20:17:58 +0300

    Add format.py

---
+++ format.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+work_dir = Path('vsl')
+
+files = []
+extensions = ['.cpp', '.h']
+for ext in extensions:
+    files += work_dir.rglob(f'*{ext}')
+
+for file in files:
+    print(file)
+    os.system(f'clang-format -i {file}')
