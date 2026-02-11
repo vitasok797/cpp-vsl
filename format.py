3c4c9ec574636c8fcff4746aef4c2a1a4f3766c2
vitasok797 <vitasok@gmail.com>
2026-02-11 19:54:57 +0300

    Update format.py script

--- format.py
+++ format.py
@@ -1,17 +1,39 @@
+from __future__ import annotations
+
 import os
 from pathlib import Path
 
+PROJECT_SRC_DIR = 'vsl'
+CLANG_FORMAT_CONFIG_PATH = None
 
-def format_in_dir(target_dir: Path) -> None:
+
+def format_in_dir(target_dir: str | Path | None) -> None:
+    if not target_dir:
+        return
+
+    target_dir = Path(target_dir)
+    if not target_dir.is_dir():
+        return
+
     files = []
     extensions = ['.cpp', '.h']
     for ext in extensions:
         files += target_dir.rglob(f'*{ext}')
 
-    for file in files:
+    for file in sorted(files):
         print(file)
-        os.system(f'clang-format -i "{file}"')
+        command = 'clang-format'
+        if CLANG_FORMAT_CONFIG_PATH:
+            command += f' --style=file:"{CLANG_FORMAT_CONFIG_PATH}"'
+        command += f' -i "{file}"'
+        os.system(command)
 
 
-format_in_dir(Path('vsl'))
-format_in_dir(Path('test'))
+def main() -> None:
+    format_in_dir(PROJECT_SRC_DIR)
+    format_in_dir('example')
+    format_in_dir('test')
+
+
+if __name__ == '__main__':
+    main()
