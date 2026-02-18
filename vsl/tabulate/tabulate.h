2df9b934a55622b4f33095146de36b9db0d1dfed
vitasok797 <vitasok@gmail.com>
2026-02-19 01:31:35 +0300

    Add create_table function to tabulate module

--- vsl/tabulate/tabulate.h
+++ vsl/tabulate/tabulate.h
@@ -3,6 +3,9 @@
 
 #include <tabulate/table.hpp>
 
+#include <ranges>
+#include <string>
+
 namespace vsl::tabulate
 {
 
@@ -21,6 +24,32 @@ inline auto hide_inner_borders(::tabulate::Table& table, HeaderType header_type)
     {
         table[1].format().show_border_top();
     }
+}
+
+auto create_table(const std::ranges::input_range auto& header,
+                  const std::ranges::input_range auto& items,
+                  auto item_to_cells) -> std::string
+{
+    auto table = ::tabulate::Table{};
+    auto row = ::tabulate::Table::Row_t{};
+    auto header_type = HeaderType::NOT_SEPARATED;
+
+    if (header.size() > 0)
+    {
+        row.assign(header.begin(), header.end());
+        table.add_row(row);
+        header_type = HeaderType::SEPARATED;
+    }
+
+    for (auto&& item : items)
+    {
+        auto cells = item_to_cells(item);
+        row.assign(cells.begin(), cells.end());
+        table.add_row(row);
+    }
+
+    hide_inner_borders(table, header_type);
+    return table.str();
 }
 
 }  // namespace vsl::tabulate
--- vsl/tabulate/tabulate.test.cpp
+++ vsl/tabulate/tabulate.test.cpp
@@ -3,20 +3,32 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <fmt/format.h>
 #include <tabulate/table.hpp>
 
-#include <format>
 #include <string>
+#include <vector>
 
 namespace vsl::tabulate::test
 {
+
+static auto header = {"ID", "Name", "Type"};
+static auto empty_header = std::vector<std::string>{};
+static auto item_to_cells = [](int x)
+{
+    return std::vector{
+        fmt::format("{}", x),
+        fmt::format("{}", x * 10),
+        fmt::format("{}", x * 100),
+    };
+};
 
 static auto get_table(int row_count, vsl::tabulate::HeaderType header_type) -> std::string
 {
     auto table = ::tabulate::Table{};
     for (auto i = int{0}; i < row_count; ++i)
     {
-        table.add_row({std::format("A{}", i), std::format("B{}", i)});
+        table.add_row({fmt::format("A{}", i), fmt::format("B{}", i)});
     }
 
     vsl::tabulate::hide_inner_borders(table, header_type);
@@ -68,6 +80,66 @@ TEST(TabulateTest, HideInnerBordersWithoutHeader)
     ASSERT_EQ(get_table(1, vsl::tabulate::HeaderType::NOT_SEPARATED), "+----+----+\n"
                                                                       "| A0 | B0 |\n"
                                                                       "+----+----+");
+}
+
+TEST(TabulateTest, CreateTable)
+{
+    auto items = {1, 2, 3, 4, 5};
+    ASSERT_EQ(create_table(header, items, item_to_cells), "+----+------+------+\n"
+                                                          "| ID | Name | Type |\n"
+                                                          "+----+------+------+\n"
+                                                          "| 1  | 10   | 100  |\n"
+                                                          "| 2  | 20   | 200  |\n"
+                                                          "| 3  | 30   | 300  |\n"
+                                                          "| 4  | 40   | 400  |\n"
+                                                          "| 5  | 50   | 500  |\n"
+                                                          "+----+------+------+");
+}
+
+TEST(TabulateTest, CreateTableNoHeader)
+{
+    auto items = {1, 2, 3, 4, 5};
+    ASSERT_EQ(create_table(empty_header, items, item_to_cells), "+---+----+-----+\n"
+                                                                "| 1 | 10 | 100 |\n"
+                                                                "| 2 | 20 | 200 |\n"
+                                                                "| 3 | 30 | 300 |\n"
+                                                                "| 4 | 40 | 400 |\n"
+                                                                "| 5 | 50 | 500 |\n"
+                                                                "+---+----+-----+");
+}
+
+TEST(TabulateTest, CreateTableOneRow)
+{
+    auto items = {1};
+    ASSERT_EQ(create_table(header, items, item_to_cells), "+----+------+------+\n"
+                                                          "| ID | Name | Type |\n"
+                                                          "+----+------+------+\n"
+                                                          "| 1  | 10   | 100  |\n"
+                                                          "+----+------+------+");
+}
+
+TEST(TabulateTest, CreateTableOneRowNoHeader)
+{
+    auto items = {1};
+    ASSERT_EQ(create_table(empty_header, items, item_to_cells), "+---+----+-----+\n"
+                                                                "| 1 | 10 | 100 |\n"
+                                                                "+---+----+-----+");
+}
+
+TEST(TabulateTest, CreateTableVariableCellCount)
+{
+    auto items = {1, 2, 3};
+    static auto item_to_cells_variable = [](int x)
+    {
+        return std::vector(x, fmt::format("{}", x));  //
+    };
+    ASSERT_EQ(create_table(header, items, item_to_cells_variable), "+----+------+------+\n"
+                                                                   "| ID | Name | Type |\n"
+                                                                   "+----+------+------+\n"
+                                                                   "| 1  |      |      |\n"
+                                                                   "| 2  | 2    |      |\n"
+                                                                   "| 3  | 3    | 3    |\n"
+                                                                   "+----+------+------+");
 }
 
 }  // namespace vsl::tabulate::test
