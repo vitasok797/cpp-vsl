1efeca8a8229c2602182f716ab55af81d20bb7ba
vitasok797 <vitasok@gmail.com>
2026-03-22 00:03:21 +0300

    Make static regex object in vsl::indent const

--- vsl/text.h
+++ vsl/text.h
@@ -18,7 +18,7 @@ inline auto indent(std::string_view text, int width) -> std::string
     // TODO: remove this POCO bug workaround
     // RE_NEWLINE_ANYCRLF cannot be used until POCO version 1.15.0
     // see https://github.com/pocoproject/poco/issues/4870
-    static auto re = Re{"^.+", Re::RE_MULTILINE | Re::RE_NEWLINE_ANY};
+    static const auto re = Re{"^.+", Re::RE_MULTILINE | Re::RE_NEWLINE_ANY};
 
     auto result = std::string{text};
     auto repl = fmt::format("{: <{}}$0", "", std::max(0, width));
