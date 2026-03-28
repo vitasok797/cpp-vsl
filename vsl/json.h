423fa79876751f2506f266d55fe367d86833711e
vitasok797 <vitasok@gmail.com>
2026-03-28 22:03:57 +0300

    Complete redesign of the JSON module

--- vsl/json.h
+++ vsl/json.h
@@ -1,35 +1,150 @@
 #ifndef VSL_JSON_H
 #define VSL_JSON_H
 
+#define JSON_USE_IMPLICIT_CONVERSIONS 0
+
+#include <fmt/format.h>
+#include <magic_enum/magic_enum.hpp>
 #include <nlohmann/json.hpp>
 
 #include <algorithm>
+#include <optional>
 #include <string>
+#include <string_view>
+#include <type_traits>
+#include <utility>
 
-#define VSL_JSON_SERIALIZE_TYPE(...) NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ONLY_SERIALIZE(__VA_ARGS__)
+#define VSL_JSON(...) NLOHMANN_DEFINE_TYPE_INTRUSIVE(__VA_ARGS__)
+#define VSL_JSON_WITH_DEFAULT(...) NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(__VA_ARGS__)
+#define VSL_JSON_ONLY_SERIALIZE(...) NLOHMANN_DEFINE_TYPE_INTRUSIVE_ONLY_SERIALIZE(__VA_ARGS__)
+
+#define VSL_JSON_TYPE(...) NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(__VA_ARGS__)
+#define VSL_JSON_TYPE_WITH_DEFAULT(...) NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(__VA_ARGS__)
+#define VSL_JSON_TYPE_ONLY_SERIALIZE(...) NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_ONLY_SERIALIZE(__VA_ARGS__)
+
+NLOHMANN_JSON_NAMESPACE_BEGIN
+
+template<typename T>
+    requires std::is_enum_v<T>
+struct adl_serializer<T>
+{
+    static void to_json(json& j, const T& e)
+    {
+        j = magic_enum::enum_contains<T>(e) ? magic_enum::enum_name(e) : "???";
+    }
+
+    static void from_json(const json& j, T& e)
+    {
+        const auto name = j.get<std::string_view>();
+        const auto value = magic_enum::enum_cast<T>(name, magic_enum::case_insensitive);
+        if (value.has_value())
+        {
+            e = value.value();
+        }
+        else
+        {
+            const auto msg = fmt::format("Unknown enum value: {}", name);
+            throw json::type_error::create(302, msg, &j);
+        }
+    }
+};
+
+NLOHMANN_JSON_NAMESPACE_END
 
 namespace vsl
 {
 
-template<typename InputType>
-auto json_from(InputType&& inp) -> auto
+using Json = nlohmann::json;
+using OrderedJson = nlohmann::ordered_json;
+
+using json_exception = nlohmann::json::exception;
+using json_parse_error = nlohmann::json::parse_error;
+using json_invalid_iterator = nlohmann::json::invalid_iterator;
+using json_type_error = nlohmann::json::type_error;
+using json_out_of_range = nlohmann::json::out_of_range;
+using json_other_error = nlohmann::json::other_error;
+
+enum class EnsureAscii
 {
-    return nlohmann::json::parse(std::forward<InputType>(inp));
+    YES,
+    NO,
+};
+
+inline auto is_valid_json_string(std::string_view str) -> bool
+{
+    return Json::accept(str);
 }
 
-template<typename Json>
-auto json_to_pretty_oneliner(const Json& json) -> std::string
+template<typename InputJsonType, typename ReferenceJsonType>
+auto json_has_extra_keys(const InputJsonType& input, const ReferenceJsonType& reference) -> bool
 {
-    auto json_str = json.dump(0);
+    if (!input.is_object() || !reference.is_object()) return false;
+    for (auto& [key, val] : input.items())
+    {
+        if (!reference.contains(key))
+        {
+            return true;
+        }
+
+        auto& ref_val = reference[key];
+        if (!val.is_object() || !ref_val.is_object()) continue;
+        if (json_has_extra_keys(val, ref_val))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename ValueType, typename JsonType>
+auto json_try_get(const JsonType& json) -> std::optional<ValueType>
+{
+    try
+    {
+        return json.get<ValueType>();
+    }
+    catch (const json_exception&)
+    {
+        return std::nullopt;
+    }
+}
+
+template<typename StructType, typename JsonType>
+    requires std::is_class_v<StructType>
+auto json_get_exact_struct(const JsonType& json) -> StructType
+{
+    auto res_struct = json.get<StructType>();
+    if (json_has_extra_keys(json, JsonType(res_struct)))
+    {
+        const auto msg = "JSON has extra unused keys";
+        throw nlohmann::json::out_of_range::create(403, msg, &json);
+    }
+    return res_struct;
+}
+
+template<typename StructType, typename JsonType>
+    requires std::is_class_v<StructType>
+auto json_try_get_exact_struct(const JsonType& json) -> std::optional<StructType>
+{
+    try
+    {
+        return json_get_exact_struct<StructType>(json);
+    }
+    catch (const json_exception&)
+    {
+        return std::nullopt;
+    }
+}
+
+template<typename JsonType>
+auto json_to_pretty_line(const JsonType& json, EnsureAscii ensure_ascii = EnsureAscii::NO) -> std::string
+{
+    constexpr auto INDENT = 0;
+    constexpr auto INDENT_CHAR = ' ';
+    auto json_str =
+        json.dump(INDENT, INDENT_CHAR, (ensure_ascii == EnsureAscii::YES), nlohmann::json::error_handler_t::replace);
     std::ranges::replace(json_str, '\n', ' ');
     return json_str;
-}
-
-template<typename Struct>
-auto struct_to_pretty_json_oneliner(const Struct& obj) -> std::string
-{
-    const auto json = nlohmann::ordered_json(obj);
-    return json_to_pretty_oneliner(json);
 }
 
 }  // namespace vsl
--- vsl/json.test.cpp
+++ vsl/json.test.cpp
@@ -3,33 +3,311 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <compare>
+#include <map>
+#include <sstream>
 #include <string>
+#include <tuple>
 
 namespace vsl::test
 {
 
-TEST(JsonTest, JsonToString)
+struct PersonInline
 {
-    const auto json_str = R"({"first_name": "John", "last_name": "Smith"})";
-    const auto json = json_from(json_str);
+    std::string name;
+    int age;
 
-    EXPECT_EQ(json_to_pretty_oneliner(json), R"({ "first_name": "John", "last_name": "Smith" })");
-}
+    auto operator<=>(const PersonInline&) const = default;
+
+    VSL_JSON(PersonInline, name, age)
+};
+
+struct PersonInlineWithDefault
+{
+    std::string name{"Noname"};
+    int age{-1};
+
+    auto operator<=>(const PersonInlineWithDefault&) const = default;
+
+    VSL_JSON_WITH_DEFAULT(PersonInlineWithDefault, name, age)
+};
+
+struct PersonInlineNonDefaultConstructible
+{
+    PersonInlineNonDefaultConstructible() = delete;
+
+    PersonInlineNonDefaultConstructible(std::string _name, int _age)
+        : name{_name},
+          age{_age}
+    {}
+
+    std::string name;
+    int age;
+
+    auto operator<=>(const PersonInlineNonDefaultConstructible&) const = default;
+
+    VSL_JSON_ONLY_SERIALIZE(PersonInlineNonDefaultConstructible, name, age)
+};
 
 struct Person
 {
-    std::string first_name;
-    std::string last_name;
+    std::string name;
     int age;
+
+    auto operator<=>(const Person&) const = default;
 };
 
-VSL_JSON_SERIALIZE_TYPE(Person, first_name, last_name, age)
-
-TEST(JsonTest, StructToJsonString)
+struct PersonWithDefault
 {
-    const auto p = Person{"John", "Smith", 20};
+    std::string name{"Noname"};
+    int age{-1};
 
-    EXPECT_EQ(struct_to_pretty_json_oneliner(p), R"({ "first_name": "John", "last_name": "Smith", "age": 20 })");
+    auto operator<=>(const PersonWithDefault&) const = default;
+};
+
+struct PersonNonDefaultConstructible
+{
+    PersonNonDefaultConstructible() = delete;
+
+    PersonNonDefaultConstructible(std::string _name, int _age)
+        : name{_name},
+          age{_age}
+    {}
+
+    std::string name;
+    int age;
+
+    auto operator<=>(const PersonNonDefaultConstructible&) const = default;
+};
+
+VSL_JSON_TYPE(Person, name, age)
+VSL_JSON_TYPE_WITH_DEFAULT(PersonWithDefault, name, age)
+VSL_JSON_TYPE_ONLY_SERIALIZE(PersonNonDefaultConstructible, name, age)
+
+static const auto PERSON_STRUCT_INLINE = PersonInline{.name = "John", .age = 20};
+static const auto PERSON_STRUCT_INLINE_DEF = PersonInlineWithDefault{.name = "John"};
+static const auto PERSON_STRUCT_INLINE_NDC = PersonInlineNonDefaultConstructible("John", 20);
+static const auto PERSON_STRUCT = Person{.name = "John", .age = 20};
+static const auto PERSON_STRUCT_DEF = PersonWithDefault{.name = "John"};
+static const auto PERSON_STRUCT_NDC = PersonNonDefaultConstructible("John", 20);
+
+static constexpr auto PERSON_STR = R"({"name":"John","age":20})";
+static constexpr auto PERSON_STR_DEF = R"({"name":"John","age":-1})";
+static constexpr auto PERSON_STR_SORTED = R"({"age":20,"name":"John"})";
+static constexpr auto PERSON_STR_SORTED_DEF = R"({"age":-1,"name":"John"})";
+
+static constexpr auto PERSON_STR_PARTIAL = R"({"name":"John"})";
+static constexpr auto PERSON_STR_BAD = R"("name": "no closing quotes)";
+
+static constexpr auto PERSON_STR_PRETTY = R"({ "name": "John", "age": 20 })";
+static constexpr auto PERSON_STR_SORTED_PRETTY = R"({ "age": 20, "name": "John" })";
+
+TEST(JsonTest, JsonCreate)
+{
+    const auto json = Json{};
+    EXPECT_TRUE(json.empty());
+
+    const auto json_ordered = OrderedJson{};
+    EXPECT_TRUE(json_ordered.empty());
+
+    const auto map = std::map<std::string, int>{
+        {"A", 1},
+        {"B", 2},
+        {"C", 3},
+    };
+    const auto json_from_map = Json(map);
+    EXPECT_EQ(json_from_map.dump(), R"({"A":1,"B":2,"C":3})");
+
+    const auto json_from_initializer_list = Json{
+        {"A", 1},
+        {"B", 2},
+    };
+    EXPECT_EQ(json_from_initializer_list.dump(), R"({"A":1,"B":2})");
+}
+
+TEST(JsonTest, JsonCreateFromStruct)
+{
+    EXPECT_EQ(Json(PERSON_STRUCT_INLINE).dump(), PERSON_STR_SORTED);
+    EXPECT_EQ(Json(PERSON_STRUCT_INLINE_DEF).dump(), PERSON_STR_SORTED_DEF);
+    EXPECT_EQ(Json(PERSON_STRUCT_INLINE_NDC).dump(), PERSON_STR_SORTED);
+
+    EXPECT_EQ(Json(PERSON_STRUCT).dump(), PERSON_STR_SORTED);
+    EXPECT_EQ(Json(PERSON_STRUCT_DEF).dump(), PERSON_STR_SORTED_DEF);
+    EXPECT_EQ(Json(PERSON_STRUCT_NDC).dump(), PERSON_STR_SORTED);
+
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT_INLINE).dump(), PERSON_STR);
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT_INLINE_DEF).dump(), PERSON_STR_DEF);
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT_INLINE_NDC).dump(), PERSON_STR);
+
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT).dump(), PERSON_STR);
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT_DEF).dump(), PERSON_STR_DEF);
+    EXPECT_EQ(OrderedJson(PERSON_STRUCT_NDC).dump(), PERSON_STR);
+}
+
+TEST(JsonTest, JsonParse)
+{
+    EXPECT_EQ(Json::parse(PERSON_STR).dump(), PERSON_STR_SORTED);
+    EXPECT_EQ(OrderedJson::parse(PERSON_STR).dump(), PERSON_STR);
+
+    std::stringstream ss1;
+    ss1 << PERSON_STR;
+    EXPECT_EQ(Json::parse(ss1).dump(), PERSON_STR_SORTED);
+
+    std::stringstream ss2;
+    ss2 << PERSON_STR;
+    EXPECT_EQ(OrderedJson::parse(ss2).dump(), PERSON_STR);
+
+    EXPECT_TRUE(Json::parse("{}").empty());
+    EXPECT_TRUE(OrderedJson::parse("{}").empty());
+
+    EXPECT_THROW(auto _ = Json::parse(PERSON_STR_BAD), json_parse_error);
+    EXPECT_THROW(auto _ = OrderedJson::parse(PERSON_STR_BAD), json_parse_error);
+}
+
+TEST(JsonTest, IsValidJsonString)
+{
+    EXPECT_TRUE(is_valid_json_string(PERSON_STR));
+    EXPECT_FALSE(is_valid_json_string(PERSON_STR_BAD));
+}
+
+TEST(JsonTest, JsonHasExtraKeys)
+{
+    {
+        const auto json = Json::parse(PERSON_STR);
+        const auto json_partial = Json::parse(PERSON_STR_PARTIAL);
+        EXPECT_TRUE(json_has_extra_keys(json, json_partial));
+        EXPECT_FALSE(json_has_extra_keys(json_partial, json));
+        EXPECT_FALSE(json_has_extra_keys(json, json));
+    }
+    {
+        const auto json = Json::parse(R"({"name":"John","sub":{"sub1":1,"sub2":2}})");
+        const auto json_partial = Json::parse(R"({"name":"John","sub":{"sub2":2}})");
+        EXPECT_TRUE(json_has_extra_keys(json, json_partial));
+        EXPECT_FALSE(json_has_extra_keys(json_partial, json));
+        EXPECT_FALSE(json_has_extra_keys(json, json));
+    }
+}
+
+TEST(JsonTest, Get)
+{
+    {
+        const auto json = Json::parse(PERSON_STR);
+        EXPECT_EQ(json["name"].get<std::string>(), PERSON_STRUCT.name);
+        EXPECT_THROW(json["name"].get<int>(), json_type_error);
+        EXPECT_THROW(json.at("unknown"), json_out_of_range);
+    }
+    {
+        const auto json = Json::parse(R"({"arr":[1,2,3],"arr_partial":[1,2]})");
+        using tuple_t = std::tuple<int, int, int>;
+        EXPECT_EQ(json["arr"].get<tuple_t>(), (tuple_t{1, 2, 3}));
+        EXPECT_THROW(json["arr_partial"].get<tuple_t>(), json_out_of_range);
+    }
+}
+
+TEST(JsonTest, GetStruct)
+{
+    const auto json = Json::parse(PERSON_STR);
+    EXPECT_EQ(json.get<PersonInline>(), PERSON_STRUCT_INLINE);
+    EXPECT_EQ(json.get<Person>(), PERSON_STRUCT);
+}
+
+TEST(JsonTest, GetStructWithDefault)
+{
+    const auto json_partial = Json::parse(R"({"name":"John","extra_key":0})");
+    EXPECT_EQ(json_partial.get<PersonInlineWithDefault>(), PERSON_STRUCT_INLINE_DEF);
+    EXPECT_EQ(json_partial.get<PersonWithDefault>(), PERSON_STRUCT_DEF);
+
+    const auto json_empty = Json::parse("{}");
+    EXPECT_EQ(json_empty.get<PersonInlineWithDefault>(), (PersonInlineWithDefault{}));
+    EXPECT_EQ(json_empty.get<PersonWithDefault>(), (PersonWithDefault{}));
+}
+
+TEST(JsonTest, GetStructPartialError)
+{
+    const auto json_partial = Json::parse(PERSON_STR_PARTIAL);
+    EXPECT_THROW(json_partial.get<PersonInline>(), json_out_of_range);
+    EXPECT_THROW(json_partial.get<Person>(), json_out_of_range);
+}
+
+TEST(JsonTest, GetStructTypeError)
+{
+    const auto json = Json::parse(R"({"name":1,"age":20})");
+    EXPECT_THROW(json.get<PersonInline>(), json_type_error);
+    EXPECT_THROW(json.get<Person>(), json_type_error);
+}
+
+TEST(JsonTest, JsonTryGet)
+{
+    const auto json = Json::parse(PERSON_STR);
+    EXPECT_THAT(json_try_get<std::string>(json["name"]), testing::Optional(std::string{"John"}));
+    EXPECT_THAT(json_try_get<Person>(json), testing::Optional(PERSON_STRUCT));
+    EXPECT_EQ(json_try_get<int>(json["name"]), std::nullopt);
+
+    const auto json_partial = Json::parse(PERSON_STR_PARTIAL);
+    EXPECT_THAT(json_try_get<PersonInlineWithDefault>(json_partial), testing::Optional(PERSON_STRUCT_INLINE_DEF));
+    EXPECT_THAT(json_try_get<PersonWithDefault>(json_partial), testing::Optional(PERSON_STRUCT_DEF));
+    EXPECT_EQ(json_try_get<PersonInline>(json_partial), std::nullopt);
+    EXPECT_EQ(json_try_get<Person>(json_partial), std::nullopt);
+}
+
+TEST(JsonTest, JsonExactStruct)
+{
+    const auto json_ok = Json(PERSON_STRUCT);
+
+    auto json_extra_key = json_ok;
+    json_extra_key["extra_key"] = 1;
+
+    auto json_wrong_type = json_ok;
+    json_wrong_type["name"] = 1;
+
+    EXPECT_EQ(json_get_exact_struct<Person>(json_ok), PERSON_STRUCT);
+    EXPECT_THAT(json_try_get_exact_struct<Person>(json_ok), testing::Optional(PERSON_STRUCT));
+
+    EXPECT_THROW(json_get_exact_struct<Person>(json_extra_key), json_out_of_range);
+    EXPECT_EQ(json_try_get_exact_struct<Person>(json_extra_key), std::nullopt);
+
+    EXPECT_THROW(json_get_exact_struct<Person>(json_wrong_type), json_type_error);
+    EXPECT_EQ(json_try_get_exact_struct<Person>(json_wrong_type), std::nullopt);
+}
+
+TEST(JsonTest, JsonToPrettyLine)
+{
+    const auto json = Json::parse(PERSON_STR);
+    EXPECT_EQ(json_to_pretty_line(json), PERSON_STR_SORTED_PRETTY);
+
+    const auto json_ord = OrderedJson::parse(PERSON_STR);
+    EXPECT_EQ(json_to_pretty_line(json_ord), PERSON_STR_PRETTY);
+
+    const auto json_non_ascii = Json::parse(R"({"val":"тест"})");
+    EXPECT_EQ(json_to_pretty_line(json_non_ascii), R"({ "val": "тест" })");
+    EXPECT_EQ(json_to_pretty_line(json_non_ascii, EnsureAscii::YES), R"({ "val": "\u0442\u0435\u0441\u0442" })");
+}
+
+TEST(JsonTest, Enum)
+{
+    enum class DemoEnum
+    {
+        OPTION_A,
+        OPTION_B,
+        OPTION_C,
+    };
+
+    auto json = Json{};
+
+    json["val"] = DemoEnum::OPTION_B;
+    EXPECT_EQ(json.dump(), R"({"val":"OPTION_B"})");
+
+    json["val"] = static_cast<DemoEnum>(2);
+    EXPECT_EQ(json.dump(), R"({"val":"OPTION_C"})");
+
+    json["val"] = static_cast<DemoEnum>(999);
+    EXPECT_EQ(json.dump(), R"({"val":"???"})");
+
+    json = Json::parse(R"({"val":"Option_B"})");
+    EXPECT_EQ(json["val"].get<DemoEnum>(), DemoEnum::OPTION_B);
+
+    json = Json::parse(R"({"val":"BAD"})");
+    EXPECT_THROW(json["val"].get<DemoEnum>(), json_type_error);
 }
 
 }  // namespace vsl::test
