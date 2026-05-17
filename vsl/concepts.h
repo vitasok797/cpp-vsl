c5a31a970d3c7888e4bb26fb6a5153388872f729
vitasok797 <vitasok@gmail.com>
2026-05-18 00:31:47 +0300

    [concepts] Add string_like concept

--- vsl/concepts.h
+++ vsl/concepts.h
@@ -3,6 +3,7 @@
 
 #include <concepts>
 #include <ranges>
+#include <string>
 #include <string_view>
 #include <type_traits>
 
@@ -25,20 +26,19 @@ template<typename T, typename U>
 concept same_type_as = std::same_as<std::decay_t<T>, U>;
 
 template<typename T>
-concept string_input_range =
-    std::ranges::input_range<T> && std::convertible_to<std::ranges::range_value_t<T>, std::string_view>;
+concept string_like = one_of<T, std::string, std::string_view, const char*, char*>;
 
 template<typename T>
-concept string_forward_range =
-    std::ranges::forward_range<T> && std::convertible_to<std::ranges::range_value_t<T>, std::string_view>;
+concept string_input_range = std::ranges::input_range<T> && string_like<std::ranges::range_value_t<T>>;
 
 template<typename T>
-concept string_bidirectional_range =
-    std::ranges::bidirectional_range<T> && std::convertible_to<std::ranges::range_value_t<T>, std::string_view>;
+concept string_forward_range = std::ranges::forward_range<T> && string_like<std::ranges::range_value_t<T>>;
 
 template<typename T>
-concept string_random_access_range =
-    std::ranges::random_access_range<T> && std::convertible_to<std::ranges::range_value_t<T>, std::string_view>;
+concept string_bidirectional_range = std::ranges::bidirectional_range<T> && string_like<std::ranges::range_value_t<T>>;
+
+template<typename T>
+concept string_random_access_range = std::ranges::random_access_range<T> && string_like<std::ranges::range_value_t<T>>;
 
 }  // namespace vsl
 
