23cac0527f99637eb2ae90f9e5d0d3c349b390f6
vitasok797 <vitasok@gmail.com>
2026-05-19 23:16:01 +0300

    [reflection] Improve fmtlib formatting support

--- vsl/reflection.h
+++ vsl/reflection.h
@@ -2,8 +2,29 @@
 #define VSL_REFLECTION_H
 
 #include <./nameof.hpp>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <cstdint>
+#include <string_view>
 
 // Reference:
 // https://github.com/Neargye/nameof/blob/master/doc/reference.md
+
+template<std::uint16_t N, typename Char>
+struct fmt::range_format_kind<nameof::cstring<N>, Char>
+    : std::integral_constant<fmt::range_format, fmt::range_format::disabled>
+{};
+
+namespace nameof
+{
+
+template<std::uint16_t N>
+auto format_as(const nameof::cstring<N>& obj) -> std::string_view
+{
+    return obj;
+}
+
+}  // namespace nameof
 
 #endif  // VSL_REFLECTION_H
--- vsl/reflection.test.cpp
+++ vsl/reflection.test.cpp
@@ -3,15 +3,25 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <fmt/format.h>
+
 namespace vsl::test
 {
 
-TEST(ReflectionTest, NameOf)
+TEST(ReflectionTest, Nameof)
 {
     const auto var = 0;
 
     EXPECT_EQ(NAMEOF(var), "var");
     EXPECT_EQ(NAMEOF_TYPE(decltype(var)), "int");
+}
+
+TEST(ReflectionTest, NameofFmt)
+{
+    const auto var = 0;
+
+    EXPECT_EQ(fmt::format("<{}>", NAMEOF(var)), "<var>");
+    EXPECT_EQ(fmt::format("<{}>", NAMEOF_SHORT_TYPE(decltype(var))), "<int>");
 }
 
 }  // namespace vsl::test
