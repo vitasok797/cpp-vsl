bf238e8c08ae65050b919757b6b4ff4d61bcc11f
vitasok797 <vitasok@gmail.com>
2026-05-24 18:16:14 +0300

    [text] Add new functions ...
    Add functions:
    - is_valid_utf8, is_ascii
    - to_casefold, to_lowercase, to_uppercase, to_titlecase
    - to_lowercase_ascii, to_uppercase_ascii
    - compare_str, compare_str_icase
    - collate, collate_icase
    - find_str, find_str_icase
    - trim_left_sv, trim_right_sv, trim_sv
    - trim_left, trim_right, trim
    - join, join_fmt
    - to_lf, to_crlf

--- vsl/text.h
+++ vsl/text.h
@@ -4,21 +4,195 @@
 #include <vsl/regex.h>
 
 #include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <uni_algo/case.h>
+#include <uni_algo/conv.h>
 
 #include <algorithm>
+#include <array>
 #include <concepts>
+#include <ranges>
 #include <string>
 #include <string_view>
 #include <utility>
 
+// Reference (uni-algo):
+// https://github.com/uni-algo/uni-algo
+// https://github.com/uni-algo/uni-algo/blob/main/doc/API.md
+
 namespace vsl
 {
 
-inline auto indent(std::string_view text, int width) -> std::string
+using FoundStr = una::found;
+
+inline auto is_valid_utf8(std::string_view str) -> bool
+{
+    return una::is_valid_utf8<char>(str);
+}
+
+inline auto is_ascii(std::string_view str) -> bool
+{
+    return std::ranges::all_of(str, [](char c) { return static_cast<unsigned char>(c) < 128; });
+}
+
+inline auto to_casefold(std::string_view str) -> std::string
+{
+    return una::cases::to_casefold_utf8<char>(str);
+}
+
+inline auto to_lowercase(std::string_view str) -> std::string
+{
+    return una::cases::to_lowercase_utf8<char>(str);
+}
+
+inline auto to_uppercase(std::string_view str) -> std::string
+{
+    return una::cases::to_uppercase_utf8<char>(str);
+}
+
+inline auto to_titlecase(std::string_view str) -> std::string
+{
+    return una::cases::to_titlecase_utf8<char>(str);
+}
+
+inline auto to_lowercase_ascii(std::string_view str) -> std::string
+{
+    auto res = std::string{};
+    res.reserve(str.size());
+
+    std::transform(str.begin(), str.end(), std::back_inserter(res),
+                   [](unsigned char c) { return (c >= 'A' && c <= 'Z') ? (c + 32) : c; });
+
+    return res;
+}
+
+inline auto to_uppercase_ascii(std::string_view str) -> std::string
+{
+    auto res = std::string{};
+    res.reserve(str.size());
+
+    std::transform(str.begin(), str.end(), std::back_inserter(res),
+                   [](unsigned char c) { return (c >= 'a' && c <= 'z') ? (c - 32) : c; });
+
+    return res;
+}
+
+inline auto compare_str(std::string_view str1, std::string_view str2) -> bool
+{
+    return (una::casesens::compare_utf8<char>(str1, str2) == 0);
+}
+
+inline auto compare_str_icase(std::string_view str1, std::string_view str2) -> bool
+{
+    return (una::caseless::compare_utf8<char>(str1, str2) == 0);
+}
+
+inline auto collate(std::string_view str1, std::string_view str2) -> int
+{
+    return una::casesens::collate_utf8<char>(str1, str2);
+}
+
+inline auto collate_icase(std::string_view str1, std::string_view str2) -> int
+{
+    return una::caseless::collate_utf8<char>(str1, str2);
+}
+
+inline auto find_str(std::string_view str1, std::string_view str2) -> FoundStr
+{
+    return una::casesens::find_utf8<char>(str1, str2);
+}
+
+inline auto find_str_icase(std::string_view str1, std::string_view str2) -> FoundStr
+{
+    return una::caseless::find_utf8<char>(str1, str2);
+}
+
+namespace detail
+{
+
+inline constexpr auto make_whitespace_lut() -> auto
+{
+    auto lut = std::array<bool, 256>{};
+    lut[static_cast<unsigned char>(' ')] = true;
+    lut[static_cast<unsigned char>('\t')] = true;
+    lut[static_cast<unsigned char>('\n')] = true;
+    lut[static_cast<unsigned char>('\r')] = true;
+    lut[static_cast<unsigned char>('\v')] = true;
+    lut[static_cast<unsigned char>('\f')] = true;
+    return lut;
+}
+
+inline constexpr auto whitespace_lut = make_whitespace_lut();
+
+inline constexpr auto is_whitespace(char c) noexcept -> bool
+{
+    return whitespace_lut[static_cast<unsigned char>(c)];
+}
+
+}  // namespace detail
+
+inline constexpr auto trim_left_sv(std::string_view str) noexcept -> std::string_view
+{
+    const auto start = std::find_if_not(str.begin(), str.end(), detail::is_whitespace);
+    return std::string_view(start, str.end());
+}
+
+inline constexpr auto trim_right_sv(std::string_view str) noexcept -> std::string_view
+{
+    const auto end = std::find_if_not(str.rbegin(), str.rend(), detail::is_whitespace).base();
+    return std::string_view(str.begin(), end);
+}
+
+inline constexpr auto trim_sv(std::string_view str) noexcept -> std::string_view
+{
+    return trim_right_sv(trim_left_sv(str));
+}
+
+inline auto trim_left(std::string_view str) -> std::string
+{
+    return std::string{trim_left_sv(str)};
+}
+
+inline auto trim_right(std::string_view str) -> std::string
+{
+    return std::string{trim_right_sv(str)};
+}
+
+inline auto trim(std::string_view str) -> std::string
+{
+    return std::string{trim_sv(str)};
+}
+
+template<std::ranges::input_range R>
+auto join(const R& items, std::string_view separator = ", ") -> std::string
+{
+    return fmt::format("{}", fmt::join(items, separator));
+}
+
+template<std::ranges::input_range R>
+auto join_fmt(const R& items, std::string_view fmt, std::string_view separator = ", ") -> std::string
+{
+    const auto full_fmt = fmt::format("{{{}}}", fmt);
+    return fmt::format(fmt::runtime(full_fmt), fmt::join(items, separator));
+}
+
+inline auto indent(std::string_view str, int width) -> std::string
 {
     static const auto re = vsl::ReAscii{"^.+", Re::multiline};
     const auto repl = std::string(std::max(0, width), ' ') + "$&";
-    return vsl::re_replace(text, re, repl);
+    return vsl::re_replace(str, re, repl);
+}
+
+inline auto to_lf(std::string_view str) -> std::string
+{
+    static const auto re = vsl::ReAscii{"\r\n"};
+    return vsl::re_replace(str, re, "\n");
+}
+
+inline auto to_crlf(std::string_view str) -> std::string
+{
+    static const auto re = vsl::ReAscii{"(?<!\r)\n"};
+    return vsl::re_replace(str, re, "\r\n");
 }
 
 template<std::integral CountType, std::integral TotalCountType>
--- vsl/text.test.cpp
+++ vsl/text.test.cpp
@@ -1,34 +1,224 @@
 #include "text.h"
 
+#include <vsl/types.h>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <cstdint>
+#include <algorithm>
+#include <string>
+#include <vector>
 
 namespace vsl::test
 {
 
+TEST(TextTest, IsValidUtf8)
+{
+    EXPECT_TRUE(vsl::is_valid_utf8(""));
+    EXPECT_TRUE(vsl::is_valid_utf8(" \n "));
+    EXPECT_TRUE(vsl::is_valid_utf8("test"));
+    EXPECT_TRUE(vsl::is_valid_utf8("тест"));
+    EXPECT_TRUE(vsl::is_valid_utf8("Straße"));
+    EXPECT_FALSE(vsl::is_valid_utf8("\xFF"));
+}
+
+TEST(TextTest, IsAscii)
+{
+    EXPECT_TRUE(vsl::is_ascii(""));
+    EXPECT_TRUE(vsl::is_ascii(" \n "));
+    EXPECT_TRUE(vsl::is_ascii("test"));
+    EXPECT_FALSE(vsl::is_ascii("тест"));
+    EXPECT_FALSE(vsl::is_ascii("Straße"));
+    EXPECT_FALSE(vsl::is_ascii("\xFF"));
+}
+
+TEST(TextTest, Case)
+{
+    EXPECT_EQ(vsl::to_casefold("LIGATURE ﬃ"), "ligature ffi");
+    EXPECT_EQ(vsl::to_lowercase("LIGATURE ﬃ"), "ligature ﬃ");
+    EXPECT_EQ(vsl::to_lowercase("ДВА"), "два");
+    EXPECT_EQ(vsl::to_uppercase("Straße"), "STRASSE");
+    EXPECT_EQ(vsl::to_titlecase("teMPuS eDAX reRuM"), "Tempus Edax Rerum");
+}
+
+TEST(TextTest, CaseAscii)
+{
+    EXPECT_EQ(vsl::to_lowercase_ascii("A_Test_Z"), "a_test_z");
+    EXPECT_EQ(vsl::to_uppercase_ascii("a_Test_z"), "A_TEST_Z");
+
+    EXPECT_EQ(vsl::to_lowercase_ascii("Тест"), "Тест");
+    EXPECT_EQ(vsl::to_uppercase_ascii("Тест"), "Тест");
+}
+
+TEST(TextTest, CompareStr)
+{
+    EXPECT_TRUE(vsl::compare_str("тест", "тест"));
+    EXPECT_FALSE(vsl::compare_str("ТЕСТ", "тест"));
+    EXPECT_TRUE(vsl::compare_str_icase("ТЕСТ", "тест"));
+
+    EXPECT_TRUE(vsl::compare_str("ﬃ", "ﬃ"));
+    EXPECT_FALSE(vsl::compare_str("ﬃ", "ffi"));
+    EXPECT_TRUE(vsl::compare_str_icase("ﬃ", "ffi"));
+
+    EXPECT_TRUE(vsl::compare_str_icase("Straße", "STRASSE"));
+    EXPECT_FALSE(vsl::compare_str_icase("тест", "текст"));
+}
+
+TEST(TextTest, Collate)
+{
+    EXPECT_EQ(vsl::collate("арбуз", "арбуз"), 0);
+    EXPECT_GT(vsl::collate("Арбуз", "арбуз"), 0);
+    EXPECT_LT(vsl::collate("арбуз", "Арбуз"), 0);
+
+    EXPECT_EQ(vsl::collate_icase("арбуз", "Арбуз"), 0);
+    EXPECT_EQ(vsl::collate_icase("Арбуз", "арбуз"), 0);
+    EXPECT_EQ(vsl::collate_icase("LIGATURE ﬃ", "ligature ffi"), 0);
+
+    auto vec = std::vector<std::string>{"браво", "астра", "АЛЬФА", "ОМЕГА"};
+    std::ranges::sort(vec);
+    EXPECT_THAT(vec, testing::ElementsAre("АЛЬФА", "ОМЕГА", "астра", "браво"));
+    std::ranges::sort(vec, [](auto a, auto b) { return vsl::collate(a, b) < 0; });
+    EXPECT_THAT(vec, testing::ElementsAre("астра", "АЛЬФА", "браво", "ОМЕГА"));
+    std::ranges::sort(vec, [](auto a, auto b) { return vsl::collate_icase(a, b) < 0; });
+    EXPECT_THAT(vec, testing::ElementsAre("АЛЬФА", "астра", "браво", "ОМЕГА"));
+}
+
+TEST(TextTest, FindStr)
+{
+    EXPECT_TRUE(vsl::find_str("раз два три", "два"));
+    EXPECT_FALSE(vsl::find_str("раз два три", "ДВА"));
+    EXPECT_TRUE(vsl::find_str_icase("раз два три", "ДВА"));
+
+    EXPECT_TRUE(vsl::find_str("Ligature ﬃ search", "ﬃ"));
+    EXPECT_FALSE(vsl::find_str("Ligature ﬃ search", "ffi"));
+    EXPECT_TRUE(vsl::find_str_icase("Ligature ﬃ search", "ffi"));
+
+    const FoundStr found = vsl::find_str_icase("Ligature ﬃ search", "ffi");
+    EXPECT_EQ(found.pos(), 9);
+    EXPECT_EQ(found.end_pos(), 12);
+}
+
+TEST(TextTest, TrimSv)
+{
+    EXPECT_EQ(vsl::trim_left_sv(""), "");
+    EXPECT_EQ(vsl::trim_right_sv(""), "");
+    EXPECT_EQ(vsl::trim_sv(""), "");
+
+    EXPECT_EQ(vsl::trim_left_sv(" \t "), "");
+    EXPECT_EQ(vsl::trim_right_sv(" \t "), "");
+    EXPECT_EQ(vsl::trim_sv(" \t "), "");
+
+    EXPECT_EQ(vsl::trim_left_sv("abc"), "abc");
+    EXPECT_EQ(vsl::trim_right_sv("abc"), "abc");
+    EXPECT_EQ(vsl::trim_sv("abc"), "abc");
+
+    EXPECT_EQ(vsl::trim_left_sv(" \t\n Тест"), "Тест");
+    EXPECT_EQ(vsl::trim_right_sv("Тест \t\n "), "Тест");
+    EXPECT_EQ(vsl::trim_sv(" \t\n Тест \t\n "), "Тест");
+
+    EXPECT_EQ(vsl::trim_left_sv("  abc  "), "abc  ");
+    EXPECT_EQ(vsl::trim_right_sv("  abc  "), "  abc");
+
+    const auto str = std::string{"  abc  "};
+    const auto trimmed_str = vsl::trim_sv(str);
+    const auto result_is_view = (trimmed_str.data() == str.data() + 2);
+    EXPECT_EQ(trimmed_str, "abc");
+    EXPECT_TRUE(result_is_view);
+}
+
+TEST(TextTest, Trim)
+{
+    EXPECT_EQ(vsl::trim_left(""), "");
+    EXPECT_EQ(vsl::trim_right(""), "");
+    EXPECT_EQ(vsl::trim(""), "");
+
+    EXPECT_EQ(vsl::trim_left(" \t "), "");
+    EXPECT_EQ(vsl::trim_right(" \t "), "");
+    EXPECT_EQ(vsl::trim(" \t "), "");
+
+    EXPECT_EQ(vsl::trim_left("abc"), "abc");
+    EXPECT_EQ(vsl::trim_right("abc"), "abc");
+    EXPECT_EQ(vsl::trim("abc"), "abc");
+
+    EXPECT_EQ(vsl::trim_left(" \t\n Тест"), "Тест");
+    EXPECT_EQ(vsl::trim_right("Тест \t\n "), "Тест");
+    EXPECT_EQ(vsl::trim(" \t\n Тест \t\n "), "Тест");
+
+    EXPECT_EQ(vsl::trim_left("  abc  "), "abc  ");
+    EXPECT_EQ(vsl::trim_right("  abc  "), "  abc");
+
+    const auto str = std::string{"  abc  "};
+    const auto trimmed_str = vsl::trim(str);
+    const auto result_is_view = (trimmed_str.data() == str.data() + 2);
+    EXPECT_EQ(trimmed_str, "abc");
+    EXPECT_FALSE(result_is_view);
+}
+
+TEST(TextTest, Join)
+{
+    const auto empty = std::vector<int>{};
+    const auto ints = std::vector{1, 2, 3};
+    const auto strings = std::vector{"abc", "def", "тест"};
+
+    EXPECT_EQ(vsl::join(empty), "");
+
+    EXPECT_EQ(vsl::join(ints), "1, 2, 3");
+    EXPECT_EQ(vsl::join(ints, "+"), "1+2+3");
+
+    EXPECT_EQ(vsl::join_fmt(ints, ":02"), "01, 02, 03");
+    EXPECT_EQ(vsl::join_fmt(ints, ":02", "+"), "01+02+03");
+
+    EXPECT_EQ(vsl::join(strings), "abc, def, тест");
+    EXPECT_EQ(vsl::join(strings, "_"), "abc_def_тест");
+
+    EXPECT_EQ(vsl::join_fmt(strings, ":?"), R"("abc", "def", "тест")");
+    EXPECT_EQ(vsl::join_fmt(strings, ":?", "_"), R"("abc"_"def"_"тест")");
+}
+
 TEST(TextTest, Indent)
 {
-    EXPECT_EQ(indent("AAA\nBBB\nCCC", 2), "  AAA\n  BBB\n  CCC");
-    EXPECT_EQ(indent("AAA\nBBB\nCCC\n", 2), "  AAA\n  BBB\n  CCC\n");
-    EXPECT_EQ(indent("AAA\r\nBBB\r\nCCC", 2), "  AAA\r\n  BBB\r\n  CCC");
-    EXPECT_EQ(indent("AAA\nBBB\nCCC", 0), "AAA\nBBB\nCCC");
-    EXPECT_EQ(indent("AAA\nBBB\nCCC", -1), "AAA\nBBB\nCCC");
-    EXPECT_EQ(indent("\n\nAAA\nBBB\n\n\nCCC\n\n", 1), "\n\n AAA\n BBB\n\n\n CCC\n\n");
-    EXPECT_EQ(indent("", 1), "");
-    EXPECT_EQ(indent(" ", 1), "  ");
-    EXPECT_EQ(indent(" \n ", 1), "  \n  ");
-    EXPECT_EQ(indent("AAA", 2), "  AAA");
-    EXPECT_EQ(indent("раз\nдва\nтри", 2), "  раз\n  два\n  три");
-    EXPECT_EQ(indent("раз\r\nдва\r\nтри", 2), "  раз\r\n  два\r\n  три");
+    EXPECT_EQ(vsl::indent("AAA\nBBB\nCCC", 2), "  AAA\n  BBB\n  CCC");
+    EXPECT_EQ(vsl::indent("AAA\nBBB\nCCC\n", 2), "  AAA\n  BBB\n  CCC\n");
+    EXPECT_EQ(vsl::indent("AAA\r\nBBB\r\nCCC", 2), "  AAA\r\n  BBB\r\n  CCC");
+    EXPECT_EQ(vsl::indent("AAA\nBBB\nCCC", 0), "AAA\nBBB\nCCC");
+    EXPECT_EQ(vsl::indent("AAA\nBBB\nCCC", -1), "AAA\nBBB\nCCC");
+    EXPECT_EQ(vsl::indent("\n\nAAA\nBBB\n\n\nCCC\n\n", 1), "\n\n AAA\n BBB\n\n\n CCC\n\n");
+    EXPECT_EQ(vsl::indent("", 1), "");
+    EXPECT_EQ(vsl::indent(" ", 1), "  ");
+    EXPECT_EQ(vsl::indent(" \n ", 1), "  \n  ");
+    EXPECT_EQ(vsl::indent("AAA", 2), "  AAA");
+    EXPECT_EQ(vsl::indent("раз\nдва\nтри", 2), "  раз\n  два\n  три");
+    EXPECT_EQ(vsl::indent("раз\r\nдва\r\nтри", 2), "  раз\r\n  два\r\n  три");
+}
+
+TEST(TextTest, LineEndings)
+{
+    const auto N = std::string{"\n"};
+    const auto RN = std::string{"\r\n"};
+
+    EXPECT_EQ(to_lf(""), "");
+    EXPECT_EQ(to_crlf(""), "");
+
+    EXPECT_EQ(to_lf(RN), N);
+    EXPECT_EQ(to_crlf(N), RN);
+
+    EXPECT_EQ(to_lf(N), N);
+    EXPECT_EQ(to_crlf(RN), RN);
+
+    EXPECT_EQ(to_lf(N + N + RN + N), (N + N + N + N));
+    EXPECT_EQ(to_crlf(RN + RN + N + RN), (RN + RN + RN + RN));
+
+    EXPECT_EQ(to_lf("abc" + RN + "def" + N + "тест" + RN + "123" + RN),
+              ("abc" + N + "def" + N + "тест" + N + "123" + N));
+    EXPECT_EQ(to_crlf("abc" + N + "def" + N + "тест" + RN + "123" + N),
+              ("abc" + RN + "def" + RN + "тест" + RN + "123" + RN));
 }
 
 TEST(TextTest, OutOf)
 {
-    EXPECT_EQ(out_of(3, 3), "3");
-    EXPECT_EQ(out_of(3, 4), "3/4");
-    EXPECT_EQ(out_of(int16_t{3}, uint64_t{4}), "3/4");
+    EXPECT_EQ(vsl::out_of(3, 3), "3");
+    EXPECT_EQ(vsl::out_of(3, 4), "3/4");
+    EXPECT_EQ(vsl::out_of(i16{3}, u64{4}), "3/4");
 }
 
 }  // namespace vsl::test
