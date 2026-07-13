eb424bbc39fc75293870eccf997793851259f2e4
vitasok797 <vitasok@gmail.com>
2026-07-13 15:15:35 +0300

    test: Update benchmark template

--- vsl/benchmark/benchmark.cpp
+++ vsl/benchmark/benchmark.cpp
@@ -1,22 +1,56 @@
 #include <./nanobench.h>
+#include <fmt/format.h>
+
+#include <cstddef>
+#include <immintrin.h>
 
 // Reference:
 // https://nanobench.ankerl.com/
 
 using namespace ankerl::nanobench;
 
-auto main() -> int
+auto flush_cache(const void* ptr, size_t size) -> void
 {
-    auto bench = Bench()
-        //.epochs(50)
-        //.minEpochIterations(1000)
-        //.relative(true)
-        ;
+    constexpr auto CACHE_LINE_SIZE = size_t{64};
+    const char* char_ptr = static_cast<const char*>(ptr);
+    for (size_t i = 0; i < size; i += CACHE_LINE_SIZE)
+    {
+        _mm_clflush(char_ptr + i);
+    }
+    _mm_mfence();
+}
 
-    bench.run("NAME",
+// --------------------------------------------------------------------------------------------------------------------
+
+// <code to test>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto test(auto&& bench, const std::string& test_name /* test data args */) -> void
+{
+    bench.run(test_name + " / OPTION",
               [&]
               {
-                  /* Your code here */
+                  // flush_cache(&data, sizeof(data));
+
+                  // <call test code>
+                  // res =
+
                   // ankerl::nanobench::doNotOptimizeAway(res);
               });
+
+    fmt::println("");
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto main() -> int
+{
+    auto bench = Bench();
+    // bench.epochs(50);
+    // bench.minEpochIterations(1000);
+    // bench.warmup(100);
+    // bench.relative(true);
+
+    test(bench, "TEST" /* test data args */);
 }
